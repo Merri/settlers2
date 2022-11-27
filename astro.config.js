de61b396dbce16312319a667d8c552c3937bfc64
Vesa Piittinen <vesa.piittinen@gmail.com>
2022-11-27 18:36:38 +0200

    Shoot in the dark

--- astro.config.js
+++ astro.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import deno from '@astrojs/deno'
 import node from '@astrojs/node'
 import preact from '@astrojs/preact'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 const isDeno = process.argv.includes('--deno')
 const isNode = !isDeno && process.argv.includes('--node')
@@ -11,7 +11,7 @@ const isVercel = !isDeno && !isNode
 export default defineConfig({
 	adapter: (isDeno && deno()) || (isNode && node()) || vercel(),
 	integrations: [preact()],
-	output: 'server',
+	output: 'static',
 	site: 'https://settlers2.net',
 	vite: {
 		optimizeDeps: {
